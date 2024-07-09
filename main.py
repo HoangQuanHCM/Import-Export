0481444b5048a80d252dc61970e2093b2a35bab8
HoangQuanHCM <nguyenhoangquan16112000@gmail.com>
2024-07-09 13:17:41 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from website import create_app
+# from website import create_app
 
-app = create_app()
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
